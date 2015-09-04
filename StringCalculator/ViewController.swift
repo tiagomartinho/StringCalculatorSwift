62b20abcc151e2c5a132281d41b176ef2cc5b9cf
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-09-04 14:49:53 +0200

    removed default template code and comments

--- StringCalculator/ViewController.swift
+++ StringCalculator/ViewController.swift
@@ -1,25 +1,5 @@
-//
-//  ViewController.swift
-//  StringCalculator
-//
-//  Created by Martinho on 03/09/15.
-//  Copyright © 2015 XPeppers. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
