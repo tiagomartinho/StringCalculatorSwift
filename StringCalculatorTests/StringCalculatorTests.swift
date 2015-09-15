750fb2eb7a272571514ef425eacbffa7f76370a1
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-09-15 14:15:24 +0200

    Revert "make tests throw error to simplify test cases and get 100% coverage"
    This reverts commit b01274e409cce15bae6d036f7f6dd5b2ca917eed.

--- StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculatorTests/StringCalculatorTests.swift
@@ -2,47 +2,43 @@ import XCTest
 @testable import StringCalculator
 
 class StringCalculatorTests: XCTestCase {
-    func testEmptyStringReturnsZero() throws {
-        try assertCalculator("",expected: 0)
+    func testEmptyStringReturnsZero() {
+        assertCalculator("",expected: 0)
     }
     
-    func testOneNumberReturnsNumber() throws {
-        try assertCalculator("1",expected: 1)
-        try assertCalculator("42",expected: 42)
+    func testOneNumberReturnsNumber() {
+        assertCalculator("1",expected: 1)
+        assertCalculator("42",expected: 42)
     }
     
-    func testAddingTwoNumbers() throws {
-        try assertCalculator("1,2",expected: 3)
+    func testAddingTwoNumbers() {
+        assertCalculator("1,2",expected: 3)
     }
     
-    func testAddingUnknownAmountOfNumbers() throws {
-        try assertCalculator("1,2,45,126,7",expected: 181)
+    func testAddingUnknownAmountOfNumbers() {
+        assertCalculator("1,2,45,126,7",expected: 181)
     }
     
-    func testAddingHandleNewLinesDelimiters() throws {
-        try assertCalculator("1\n2,3",expected: 6)
+    func testAddingHandleNewLinesDelimiters() {
+        assertCalculator("1\n2,3",expected: 6)
     }
     
-    func testSupportDifferentDelimeters() throws {
-        try assertCalculator("//;\n1;2",expected: 3)
+    func testSupportDifferentDelimeters(){
+        assertCalculator("//;\n1;2",expected: 3)
     }
     
-    func testAddWithNegativeNumbersThrowsException() throws {
-        let result = try assertCalculator("-1,2",expected: 3)
-        XCTAssertEqual(result,[-1])
+    func testAddWithNegativeNumbersThrowsException(){
+        XCTAssertEqual(assertCalculator("-1,2",expected: 0),[-1])
     }
     
-    func testAddWithoutNegativeNumbersDoesNotThrowsException() throws {
-        let result = try assertCalculator("1,2",expected: 3)
-        XCTAssertEqual(result,[])
-    }
-    
-    func assertCalculator(input:String,expected:Int) throws ->[Int]{
+    func assertCalculator(input:String,expected:Int)->[Int]{
         do {
             let result = try StringCalculator.add(input)
             XCTAssertEqual(result, expected)
         } catch StringCalculatorError.NegativeNotAllowed(let negativeNumbers){
             return negativeNumbers
+        }
+        catch {
         }
         return [Int]()
     }
