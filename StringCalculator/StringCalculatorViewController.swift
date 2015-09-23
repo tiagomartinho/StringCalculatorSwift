99fd94354a922baf7fbf08619c2a375f3734a154
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-09-23 14:14:06 +0200

    renamed view controller to StringCalculatorViewController connect UI with view controller

---
+++ StringCalculator/StringCalculatorViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class StringCalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var stringInput: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func calculate() {
+        
+    }
+}
+
