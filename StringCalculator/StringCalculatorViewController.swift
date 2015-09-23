53bd585ad59cf45204e3397c68148da6ffb9939f
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-09-23 14:18:09 +0200

    use StringCalculator to calculate result and assign it to label text

--- StringCalculator/StringCalculatorViewController.swift
+++ StringCalculator/StringCalculatorViewController.swift
@@ -7,7 +7,12 @@ class StringCalculatorViewController: UIViewController {
     @IBOutlet weak var result: UILabel!
     
     @IBAction func calculate() {
-        
+        let calculator = StringCalculator(numbers: stringInput.text ?? "")
+        do {
+            result.text = "\(try calculator.add())"
+        }
+        catch {
+        }
     }
 }
 
