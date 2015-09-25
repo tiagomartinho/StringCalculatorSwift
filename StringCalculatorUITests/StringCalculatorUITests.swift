42d5f6dda0d319a84394a408d3b0db907591be5a
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-09-25 14:29:22 +0200

    add UI test to see if all elements are accessible and with the correct value

--- StringCalculatorUITests/StringCalculatorUITests.swift
+++ StringCalculatorUITests/StringCalculatorUITests.swift
@@ -1,4 +1,31 @@
 import XCTest
 
 class StringCalculatorUITests: XCTestCase {
+    
+    let application = XCUIApplication()
+    
+    var calculateButton: XCUIElement?
+    var stringInputTextField: XCUIElement?
+    var resultInformationLabel: XCUIElement?
+    var resultLabel: XCUIElement?
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        application.launch()
+        
+        calculateButton = application.buttons["calculate"]
+        stringInputTextField = application.textFields["stringInput"]
+        resultInformationLabel = application.staticTexts.elementMatchingType(.Any, identifier: "resultLabel")
+        resultLabel = application.staticTexts.elementMatchingType(.Any, identifier: "result")
+    }
+    
+    func testCheckUIAndIsAccessible() {
+        XCTAssert(calculateButton?.exists ?? false)
+        XCTAssert(stringInputTextField?.exists ?? false)
+        XCTAssert(resultInformationLabel?.exists ?? false)
+        XCTAssert(resultLabel?.exists ?? false)
+        XCTAssertEqual("",stringInputTextField?.value as? String)
+        XCTAssertEqual("",resultLabel?.label)
+    }
 }
