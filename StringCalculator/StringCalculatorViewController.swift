ad2be5d5a26f139dc1eb39c621a9a167ed0ff94b
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-09-29 14:25:39 +0200

    add pod progress hud, do string calculation in background to show HUD

--- StringCalculator/StringCalculatorViewController.swift
+++ StringCalculator/StringCalculatorViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SVProgressHUD
 
 class StringCalculatorViewController: UIViewController {
     
@@ -7,14 +8,46 @@ class StringCalculatorViewController: UIViewController {
     @IBOutlet weak var result: UILabel!
     
     @IBAction func calculate() {
-        let calculator = StringCalculator(numbers: stringInput.text ?? "")
+        showLoadingInterface()
+        executeInBackground {
+           let result = self.stringCalculatorResult(self.stringInput.text)
+            self.executeInMainThread {
+                self.hideLoadingInterface()
+                self.result.text = result
+            }
+        }
+    }
+    
+    func executeInBackground(block:Void->Void){
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            block()
+        }
+    }
+    
+    func executeInMainThread(block:Void->Void){
+        dispatch_async(dispatch_get_main_queue()) {
+            block()
+        }
+    }
+    
+    func stringCalculatorResult(input:String?)->String{
+        let calculator = StringCalculator(numbers: input ?? "")
         do {
-            result.text = "\(try calculator.add())"
+            return "\(try calculator.add())"
         } catch StringCalculatorError.NegativeNotAllowed(let negativeNumbers){
-            result.text = "Negative numbers are not allowed. Negative Numbers: \(negativeNumbers)"
+            return "Negative numbers are not allowed. Negative Numbers: \(negativeNumbers)"
         }
         catch {
+            return ""
         }
+    }
+    
+    func showLoadingInterface(){
+        SVProgressHUD.show()
+    }
+    
+    func hideLoadingInterface(){
+        SVProgressHUD.dismiss()
     }
 }
 
