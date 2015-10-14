af70a5d057d7f748f1b0d877cd79b6a048f4141b
tiagomartinho <tiago.martinho@xpeppers.com>
2015-10-14 14:17:41 +0200

    add fabric and crashlytics support

--- StringCalculator/AppDelegate.swift
+++ StringCalculator/AppDelegate.swift
@@ -1,7 +1,14 @@
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics.self()])
+        return true
+    }
 }
 
